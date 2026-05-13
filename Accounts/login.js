065ff285b464122673392c5bb0f77035ad2c1724
Jacketylon <johnikruger293@gmail.com>
2026-05-13 18:22:08 +0200

    What I have right now

---
+++ Accounts/login.js
@@ -0,0 +1,23 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const response = await fetch("/register", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    });
+
+    const data = await response.json();
+
+    console.log(data);
+});
